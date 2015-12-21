cd966270901e508ae613e6a84b4f68da7df40705
Konstantin Kavruk <6k6k6k@gmail.com>
2015-12-21 15:51:07 +0200

    Update 1wirekeyboard.ino

--- 1wirekeyboard.ino
+++ 1wirekeyboard.ino
@@ -1,7 +1,8 @@
 #define MAX_NUMBER 25000
 #define ENTER_KEY 41
 #define DEL_KEY 42
-#define NOT_PRESSED_THRESHOLD 252
+#define NOT_PRESSED_THRESHOLD 25
+#define NO_KEY_PRESSED 255
 int keypressed = 0;
 int keyboardPin = 7;    // Analog input pin that the keypad is attached to
 int keyboardValue = 0;   // value read from the keyboard
@@ -14,7 +15,6 @@ LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
 #define SLEEP 7     // Pin 7 connected to SLEEP pin
 #define ZERO_POS 2//pin punct 0 
 #define MAX_STEPS  27000//se va inmulti cu 1000
-int direction;    // Variable to set Rotation (CW-CCW) of the motor
 int currentPos=0,targetPos=0;
 // variable to store the length of received bytes
 int incomingLen = 0;
@@ -43,86 +43,56 @@ int strToInt(char AStr[], byte ALen)
   return Result;
 }
 void setup() {
-   pinMode(dir_pin, OUTPUT);
-   pinMode(step_pin, OUTPUT);
-   pinMode(SLEEP, OUTPUT);
-   Serial.begin(9600);
- 
+  Serial.begin(9600);
   lcd.begin(20, 4);
   lcd.setCursor(1,0);
-  lcd.print("Magnetic Meter");
-  lcd.setCursor(0,1);
-  lcd.print("RaceAutoElectro");
-  delay (2000);
- 
   Serial.setTimeout(2000);
-  digitalWrite(SLEEP, HIGH);  // Wake up EasyDriver
-  delay(5);  // Wait for EasyDriver wake up
-   
- 
-
-
-
-   while (!digitalRead(ZERO_POS)){
-    digitalWrite(dir_pin,HIGH);
-    digitalWrite(step_pin, HIGH);
-    delay(1);
-    digitalWrite(step_pin, LOW);
-    delay(1);   //delay for reference point - BACK
-   }
-   
-    while (digitalRead(ZERO_POS)){
-    digitalWrite(dir_pin,LOW);
-    digitalWrite(step_pin, HIGH);
-    delay(1);
-    digitalWrite(step_pin, LOW);
-    delay(1);   //delay for reference point - FORWARD    ---0 REFERENCE---
-    
-   }
 }
 void readkeyboard(){
      keyboardValue = analogRead(keyboardPin); // read the value (0-1023)
-   if (keyboardValue <NOT_PRESSED_THRESHOLD){ //minimum press threshold
-      delay(30);           //30ms delay for debounde
-      keyboardValue=0;
-      for(char i=0;i<5;i++) //read 5 samples 
+  if (keyboardValue > NOT_PRESSED_THRESHOLD){ //minimum press threshold
+    delay(30);           //30ms delay for debounde
+    keyboardValue=0;
+    for(char i=0;i<5;i++) //read 5 samples 
         keyboardValue+=analogRead(keyboardPin);
-      keyboardValue=keyboardValue/5;  //calculate 5 sample average
-   }
-     
- if ((keyboardValue >70) && (keyboardValue < 85)){keypressed = ENTER_KEY;}
- if ((keyboardValue >145) && (keyboardValue < 156)){keypressed = 7;}
- if ((keyboardValue >220) && (keyboardValue < 228)){keypressed = 4;}
- if ((keyboardValue >280) && (keyboardValue < 290)){keypressed = 1;}
- if ((keyboardValue >350) && (keyboardValue < 360)){keypressed = 0;}
- if ((keyboardValue >420) && (keyboardValue < 430)){keypressed = 8;}
- if ((keyboardValue >490) && (keyboardValue < 500)){keypressed = 5;}
- if ((keyboardValue >570) && (keyboardValue < 580)){keypressed = 2;}
- if ((keyboardValue >660) && (keyboardValue < 670)){keypressed = DEL_KEY;}
- if ((keyboardValue >750) && (keyboardValue < 770)){keypressed = 9;}
- if ((keyboardValue >870) && (keyboardValue < 890)){keypressed = 6;}
- if ((keyboardValue >1010) && (keyboardValue < 1024)){keypressed = 3;}
- 
-  
-  //NOTE: the values used above are all halfway between the value obtained with each keypress in previous test sketch 
-  if (analogRead(keyboardPin) > NOT_PRESSED_THRESHOLD){
-    int temp = composedNumber;
-    composedNumber*=10;
-    composedNumber+=keypressed;
-    if (composedNumber>MAX_NUMBER || composedNumber<0){
-      composedNumber=temp;
-      Serial.println("Error: Out of bounds!");
+    keyboardValue=keyboardValue/5;  //calculate 5 sample average
+  }
+  //keypressed=NO_KEY_PRESSED;    
+  if ((keyboardValue >70) && (keyboardValue < 90)){keypressed = ENTER_KEY;}
+  if ((keyboardValue >145) && (keyboardValue < 156)){keypressed = 7;}
+  if ((keyboardValue >220) && (keyboardValue < 228)){keypressed = 4;}
+  if ((keyboardValue >280) && (keyboardValue < 290)){keypressed = 1;}
+  if ((keyboardValue >350) && (keyboardValue < 360)){keypressed = 0;}
+  if ((keyboardValue >420) && (keyboardValue < 430)){keypressed = 8;}
+  if ((keyboardValue >490) && (keyboardValue < 500)){keypressed = 5;}
+  if ((keyboardValue >570) && (keyboardValue < 580)){keypressed = 2;}
+  if ((keyboardValue >660) && (keyboardValue < 670)){keypressed = DEL_KEY;}
+  if ((keyboardValue >750) && (keyboardValue < 770)){keypressed = 9;}
+  if ((keyboardValue >870) && (keyboardValue < 890)){keypressed = 6;}
+  if ((keyboardValue >1010) && (keyboardValue < 1024)){keypressed = 3;}
+  Serial.println(keypressed);
+  //if (255!=keypressed) {
+    //NOTE: the values used above are all halfway between the value obtained with each keypress in previous test sketch 
+    if (analogRead(keyboardPin) > NOT_PRESSED_THRESHOLD){
+      int temp = composedNumber;
+      composedNumber*=10;
+      composedNumber+=keypressed;
+      if (composedNumber>MAX_NUMBER || composedNumber<0){
+        composedNumber=temp;
+        Serial.println("Error: Out of bounds!");
       }
-    lcd.clear();
-    lcd.print(composedNumber);
-    if (keypressed==DEL_KEY)                  //clear the current number
-      composedNumber=0;
-    if(keypressed==ENTER_KEY) {               //set the target to current number
-      targetPos=(composedNumber-2550)*1.3333;
-      Serial.print("ENTER pressed, target set to"+targetPos);
+      lcd.clear();
+      lcd.print(composedNumber);
+       
+      if (ENTER_KEY==keypressed)  {                //set the target to current number
+        Serial.println("ENTER pressed");
+      }
+      if (DEL_KEY==keypressed)  {                //clear the current number
+        Serial.println("DELETE pressed");
+        composedNumber=0;
+      }
     }
-    }
-      
+  //}  
   while (analogRead(keyboardPin) > NOT_PRESSED_THRESHOLD) { //wait until key no longer pressed
   }
 
@@ -144,43 +114,18 @@ void loop() {
     {
       // convert the ascii string number into correct binary form
       targetPos=(strToInt(buffer, readLen)-2550)*1.3333;
-     
       // notify sender of the entered value
       Serial.print("Entered value: ");
       Serial.println(targetPos/1.333+2550);
-       lcd.clear();
+      lcd.clear();
       lcd.print ("value:");
       lcd.setCursor(6,0);
       lcd.print (targetPos/1.333+2550, DEC);
-
-     
-     
       // Print extra empty line
       Serial.println("");
     }
       Serial.println("empty");
-   
   }
 
-  while (targetPos>0 && targetPos<MAX_STEPS && !digitalRead(ZERO_POS) && currentPos!=targetPos) {
-    if (currentPos>targetPos) {
-      
-      digitalWrite(dir_pin, HIGH);  // (HIGH = anti-clockwise / LOW = clockwise)
-      digitalWrite(step_pin, HIGH);
-     // delay(1);
-      digitalWrite(step_pin, LOW);
-      //delay(1);
-      for (int i=0;i<DELAY_TIME;i++) analogRead(A6);
-      currentPos--;
-    }
-    if (currentPos<targetPos) {
-      digitalWrite(dir_pin, LOW);  // (HIGH = anti-clockwise / LOW = clockwise)
-      digitalWrite(step_pin, HIGH);
-      //delay(1);
-      digitalWrite(step_pin, LOW);
-     //delay(1);
-     for (int i=0;i<DELAY_TIME;i++) analogRead(A6);
-      currentPos++;
-    }
-  } 
+
  }
