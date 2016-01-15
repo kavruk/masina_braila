17cef9374a0c0e68bb07a4265adda62a11507de1
Konstantin Kavruk <6k6k6k@gmail.com>
2016-01-15 19:38:54 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -207,8 +207,7 @@ void loop() {
     for (int i=0;i<4;i++) {
       if(i>0)
         targetPos[i]-=2666*i;
-        
-      while (targetPos[i]>0 && targetPos[i]<MAX_NUMBER && !digitalRead(ZERO_POS) && currentPos!=targetPos[i]) {
+      while (targetPos[i]<MAX_NUMBER && !digitalRead(ZERO_POS) && currentPos!=targetPos[i]) {//removed zero condition
 #if SERIAL_ENABLED
         Serial.print(currentPos);
         Serial.print(":");
