43765181ac689c1f712f15c63b58719c1739bcde
RaceAutoElectro <raceautoelectro@gmail.com>
2016-01-16 14:19:15 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,148 +1,288 @@
-#include <Wire.h>
-#define DELAY_TIME 1
-#include <OnewireKeypad.h>
-char KEYS[]= {
-  '7','8','9','+','a','b',   //'A',
-  '4','5','6','c','<','d',   //'B',
-  '1','2','3','e','f','g',   //'C'
-  'Z','0','C','.','h','j',   //'D',
-  '4','o','6','*','k','l',   //'E',
-  'z','x','c','v','-','q',   //'F'
-                }; 
-OnewireKeypad <Print, 36 > KP2(Serial, KEYS, 6, 6, A6, 4700, 1000 );
-
+//keyboard
+#define MAX_NUMBER 25000
+#define MIN_NUMBER 2560
+#define ENTER_KEY 41
+#define DEL_KEY 42
+#define NOT_PRESSED_THRESHOLD 25
+#define NO_KEY_PRESSED 255
+#define SERIAL_ENABLED 0
+#define ZERO_POS 2//pin punct 0 
+#define CLEAR_20CM 2666
+#define SAMPLES_NUMBER 5
+int keypressed = 0;
+int keyboardPin = 6;    // Analog input pin that the keypad is attached to 0 la test ... 7 pe masina 
+int keyboardValue = 0;   // value read from the keyboard
+int composedNumber[4];
+int targetPos[4];
+int numberOfMeasurement=0;
+#define CUT_ENGAGED 12
+#define CUT_EXECUTED 13
+#define CLEAR_20CM 2000
+//display
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
+LiquidCrystal lcd(A0, A1, A2, A3, A4, A5); /// 11, 12 pt test  a1 , a2 pe masina 
+//motor
 #define step_pin 9 // Pin 3 connected to Steps pin on EasyDriver
 #define dir_pin 10   // Pin 2 connected to Direction pin
 #define SLEEP 7     // Pin 7 connected to SLEEP pin
-#define ZERO_POS 2//pin punct 0 
-#define MAX_STEPS  33320//se va inmulti cu 1000
-int direction;    // Variable to set Rotation (CW-CCW) of the motor
-int currentPos=0,targetPos=0;
+#define DELAY_TIME 2   // 1 pe masina .....8 pt test 
+int currentPos=0;
+
+//serial
+#if SERIAL_ENABLED
 // variable to store the length of received bytes
 int incomingLen = 0;
 // variable to store the actual bytes read from the receive buffer
 byte readLen = 0;
 // variable to store incoming bytes of ascii chars
 char buffer[64];
-
-
 int strToInt(char AStr[], byte ALen)
 {
   int Result = 0;
   int c;
-  for (int i=0; i < ALen; i++)
+  for (int i = 0; i < ALen; i++)
   {
     // calc the ordinal value of each character
     c = int(AStr[i] - '0');
     // check if the current char is number, otherwise return -1
     if (c < 0 || c > 9)
       return -1;
-   
+
     // adjust the prior result and add the ordinal value of
-    // the current char 
+    // the current char
     Result = (Result * 10) + c;
   }
   return Result;
 }
+#endif
 void setup() {
-   pinMode(dir_pin, OUTPUT);
-   pinMode(step_pin, OUTPUT);
-   pinMode(SLEEP, OUTPUT);
-   Serial.begin(9600);
- 
-  lcd.begin(20, 4);
-  lcd.setCursor(1,0);
-  lcd.print("Magnetic Meter");
-  lcd.setCursor(0,1);
-  lcd.print("RaceAutoElectro");
-  delay (2000);
- 
-   Serial.setTimeout(2000);
-   digitalWrite(SLEEP, HIGH);  // Wake up EasyDriver
-   delay(5);  // Wait for EasyDriver wake up
-   
- 
-
-
-
-   while (!digitalRead(ZERO_POS)){
+  pinMode(dir_pin, OUTPUT);
+  pinMode(step_pin, OUTPUT);
+  pinMode(SLEEP, OUTPUT);
+  digitalWrite(SLEEP, HIGH);  // Wake up EasyDriver
+  delay(5);  // Wait for EasyDriver wake up
+  while (!digitalRead(ZERO_POS)){      //MOVE to zero
     digitalWrite(dir_pin,HIGH);
     digitalWrite(step_pin, HIGH);
     delay(1);
     digitalWrite(step_pin, LOW);
     delay(1);   //delay for reference point - BACK
-    
    }
-   
-    while (digitalRead(ZERO_POS)){
+   while (digitalRead(ZERO_POS)){     //MOVE forward until not touching 0 point any more
     digitalWrite(dir_pin,LOW);
     digitalWrite(step_pin, HIGH);
     delay(1);
     digitalWrite(step_pin, LOW);
     delay(1);   //delay for reference point - FORWARD    ---0 REFERENCE---
-    
    }
+#if SERIAL_ENABLED
+  Serial.begin(9600);
+  Serial.setTimeout(2000);
+#endif
+  lcd.begin(20, 4);
+  lcd.setCursor(0, 0);
+  lcd.blink();
+  for (int i=0;i<4;i++){
+    composedNumber[i]=0;
+    targetPos[i]=0;
+  }
 }
- 
+void readkeyboard() {
+  keyboardValue = analogRead(keyboardPin); // read the value (0-1023)
+  if (keyboardValue > NOT_PRESSED_THRESHOLD) { //minimum press threshold
+    delay(30);           //30ms delay for debounce
+    keyboardValue = 0;
+    for (char i = 0; i < SAMPLES_NUMBER; i++) //read 5 samples
+      keyboardValue += analogRead(keyboardPin);
+    keyboardValue = keyboardValue / SAMPLES_NUMBER; //calculate 5 sample average
+  }
+  keypressed=NO_KEY_PRESSED;
+  if ((keyboardValue >70) && (keyboardValue < 90)){keypressed = ENTER_KEY;}
+  if ((keyboardValue >145) && (keyboardValue < 156)){keypressed = 7;}
+  if ((keyboardValue >220) && (keyboardValue < 228)){keypressed = 4;}
+  if ((keyboardValue >280) && (keyboardValue < 290)){keypressed = 1;}
+  if ((keyboardValue >350) && (keyboardValue < 360)){keypressed = 0;}
+  if ((keyboardValue >420) && (keyboardValue < 430)){keypressed = 8;}
+  if ((keyboardValue >490) && (keyboardValue < 500)){keypressed = 5;}
+  if ((keyboardValue >570) && (keyboardValue < 582)){keypressed = 2;}
+  if ((keyboardValue >660) && (keyboardValue < 672)){keypressed = DEL_KEY;}
+  if ((keyboardValue >750) && (keyboardValue < 770)){keypressed = 9;}
+  if ((keyboardValue >870) && (keyboardValue < 890)){keypressed = 6;}
+  if ((keyboardValue >1010) && (keyboardValue < 1024)){keypressed = 3;}
+  Serial.println(keyboardValue);
+  if (keypressed>=0&&keypressed<=9) {     //create multi char number from key presses
+    int temp = composedNumber[numberOfMeasurement];
+    composedNumber[numberOfMeasurement]*=10;
+    composedNumber[numberOfMeasurement]+=keypressed;
+    if (composedNumber[numberOfMeasurement]>MAX_NUMBER || composedNumber[numberOfMeasurement]<0){   //oare trebuie bagata si valoare minima??? 20cm?
+      composedNumber[numberOfMeasurement]=temp;
+#if SERIAL_ENABLED
+      Serial.println("Error: Out of bounds!");
+#endif
+     }
+     lcd.setCursor(0,numberOfMeasurement);
+     targetPos[numberOfMeasurement]=(composedNumber[numberOfMeasurement]-2550)*1.333;
+     lcd.print(composedNumber[numberOfMeasurement]);
+#if SERIAL_ENABLED
+      Serial.println(composedNumber[numberOfMeasurement]);
+#endif
+  }
+  if (ENTER_KEY==keypressed){                //set the target to current number
+#if SERIAL_ENABLED
+    Serial.println("ENTER pressed");
+#endif
+  if (composedNumber[numberOfMeasurement]<MIN_NUMBER){
+#if SERIAL_ENABLED
+        Serial.print("Error: Number smaller than ");
+        Serial.println(MIN_NUMBER);
+#endif
+        composedNumber[numberOfMeasurement]=0;
+        targetPos[numberOfMeasurement]=0;
+        lcd.setCursor(0,numberOfMeasurement);
+        lcd.print("Numar prea mic");
+        delay(500);
+        deleteRow();
+        if (numberOfMeasurement>=0)
+           numberOfMeasurement--;  
+     }
+  numberOfMeasurement++;
+#if SERIAL_ENABLED
+    Serial.print("number of measurement = ");
+    Serial.println(numberOfMeasurement);
+#endif
+  if (5==numberOfMeasurement){            
+        numberOfMeasurement=0;
+        for(int i=0;i<4;i++){
+          composedNumber[i]=0;      //clear all stored readings
+          targetPos[i]=0;
+          lcd.clear();
+         lcd.setCursor(0,0);
+        }
+      }
+    lcd.setCursor(0, numberOfMeasurement);
+  }
+  if (DEL_KEY==keypressed)  {                //clear the current number
+#if SERIAL_ENABLED
+    Serial.println("DELETE pressed");
+#endif
+    deleteRow();
+    if (numberOfMeasurement>0)
+      numberOfMeasurement--;
+  }
+  while (analogRead(keyboardPin) > NOT_PRESSED_THRESHOLD) { //wait until key no longer pressed
+  }
+}
 void loop() {
+  readkeyboard(); //get the value of key being pressed "keypressed" i.e. 0-9
+#if SERIAL_ENABLED
   incomingLen = Serial.available();
-  if (incomingLen==0)
+  if (incomingLen == 0)
   {
-    // if there is no data in serial input buffer, let's sleep
-    // for a while
+    // if there is no data in serial input buffer, let's sleep for a while
     delay(100);
   }
-  else
-  {
+  else {
     // read the data in serial input buffer
     readLen = Serial.readBytes(buffer, incomingLen);
-    if (readLen!=0)
-    {
+    if (readLen != 0){
       // convert the ascii string number into correct binary form
-      targetPos=(strToInt(buffer, readLen)-2550)*1.333;
-      //targetPos=strToInt(buffer, readLen)*1.333;
+      composedNumber[numberOfMeasurement] = strToInt(buffer, readLen);
+      targetPos[numberOfMeasurement]=(composedNumber[numberOfMeasurement]-2550)*1.3333;
       // notify sender of the entered value
       Serial.print("Entered value: ");
-      Serial.println((targetPos+2550)/1.333);
-       lcd.clear();
-      lcd.print ("value:");
-      lcd.setCursor(6,0);
-      lcd.print ((targetPos+2550)/1.3333, DEC);
-
-     
-     
+      Serial.println(composedNumber[numberOfMeasurement]);
       // Print extra empty line
       Serial.println("");
-    }
-    else {
-      Serial.println("empty");
+      lcd.setCursor(0, numberOfMeasurement);
+      lcd.print(composedNumber[numberOfMeasurement]);
+      numberOfMeasurement++;
     }
   }
-
-
-  
-    
-  while (targetPos>0 && targetPos<MAX_STEPS && !digitalRead(ZERO_POS) && currentPos!=targetPos) {
-    if (currentPos>targetPos) {
-      
-      digitalWrite(dir_pin, HIGH);  // (HIGH = anti-clockwise / LOW = clockwise)
-      digitalWrite(step_pin, HIGH);
-     // delay(1);
-      digitalWrite(step_pin, LOW);
-      //delay(1);
-      for (int i=0;i<DELAY_TIME;i++) analogRead(A6);
-      currentPos--;
-    }
-    if (currentPos<targetPos) {
+#endif    
+      //move motor
+  if (4==numberOfMeasurement){
+#if SERIAL_ENABLED        
+    Serial.println("4 measurements entered");
+#endif        
+    for (int i=0;i<4;i++) {
+      if(i>0)
+        targetPos[i]-=2666*i;
+      while (targetPos[i]<MAX_NUMBER && !digitalRead(ZERO_POS) && currentPos!=targetPos[i]) {//removed zero condition
+#if SERIAL_ENABLED
+        Serial.print(currentPos);
+        Serial.print(":");
+        Serial.println(composedNumber[i]);
+        Serial.println(digitalRead(CUT_ENGAGED));
+        Serial.println(digitalRead(CUT_EXECUTED));
+#endif 
+        if (currentPos>targetPos[i]) {
+          digitalWrite(dir_pin, HIGH);  // (HIGH = anti-clockwise / LOW = clockwise)
+          digitalWrite(step_pin, HIGH);
+          // delay(1);
+          digitalWrite(step_pin, LOW);
+          //delay(1);
+          for (int i=0;i<DELAY_TIME;i++) analogRead(A7);
+            currentPos--;
+        }
+        if (currentPos<targetPos[i]) {
+          digitalWrite(dir_pin, LOW);  // (HIGH = anti-clockwise / LOW = clockwise)
+          digitalWrite(step_pin, HIGH);
+          //delay(1);
+          digitalWrite(step_pin, LOW);
+          //delay(1);
+          for (int i=0;i<DELAY_TIME;i++) 
+            analogRead(A7);
+          currentPos++;
+        }
+      }
+      while(digitalRead(CUT_ENGAGED)==1);
+      while(digitalRead(CUT_EXECUTED)==0);
+      while(digitalRead(CUT_ENGAGED)==1);
+      delay(2000);
+      //move forward 20cm
       digitalWrite(dir_pin, LOW);  // (HIGH = anti-clockwise / LOW = clockwise)
-      digitalWrite(step_pin, HIGH);
-      //delay(1);
-      digitalWrite(step_pin, LOW);
-     //delay(1);
-     for (int i=0;i<DELAY_TIME;i++) analogRead(A6);
-      currentPos++;
+      for (int i=0;i<2666;i++){
+        digitalWrite(step_pin, HIGH);
+        delay(1);
+        digitalWrite(step_pin, LOW);
+        delay(1);
+      }   
+      lcd.setCursor(7,i);
+      lcd.print("ia bagheta");
+      delay(2000);
+      lcd.setCursor(7,i);
+      lcd.print("terminat  ");
     }
-  } 
- }
+    delay(500);
+    lcd.clear();
+     while (!digitalRead(ZERO_POS)){      //MOVE to zero
+    digitalWrite(dir_pin,HIGH);
+    digitalWrite(step_pin, HIGH);
+    for (int i=0;i<DELAY_TIME;i++) 
+            analogRead(A7);
+    digitalWrite(step_pin, LOW);
+    for (int i=0;i<DELAY_TIME;i++) 
+            analogRead(A7);   //delay for reference point - BACK
+   }
+   while (digitalRead(ZERO_POS)){     //MOVE forward until not touching 0 point any more
+    digitalWrite(dir_pin,LOW);
+    digitalWrite(step_pin, HIGH);
+    delay(1);
+    digitalWrite(step_pin, LOW);
+    delay(1);
+   }
+   currentPos=0;
+    numberOfMeasurement=0;
+    for(int i=0;i<4;i++){
+      composedNumber[i]=0;      //clear all stored readings
+      targetPos[i]=0;
+    }
+  }
+}
+void deleteRow(){
+  lcd.setCursor(0,numberOfMeasurement);
+  lcd.print("              ");
+  lcd.setCursor(0,numberOfMeasurement);
+  composedNumber[numberOfMeasurement]=0;
+  targetPos[numberOfMeasurement]=0;
+}
