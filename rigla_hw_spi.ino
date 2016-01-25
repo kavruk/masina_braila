357f0c4bd0886b4e8e5986cb57a1c7703fd0779f
Konstantin Kavruk <6k6k6k@gmail.com>
2016-01-25 18:23:47 +0200

    Update rigla_hw_spi.ino

--- rigla_hw_spi.ino
+++ rigla_hw_spi.ino
@@ -3,18 +3,17 @@
 #define DATAIN  12//MISO 
 #define SPICLOCK  13//sck
 #define SLAVESELECT 10//ss
-byte eeprom_input_data = 0;
-byte clr;
-int address = 0;
-char spi_transfer(volatile char data)
-{
-  SPDR = data;                    // Start the transmission
-  Serial.println("transmissionstart");
-  while (!(SPSR & (1 << SPIF)))   // Wait the end of the transmission
-  {
-    Serial.println(SPDR);
-  };
-  return SPDR;                    // return the received byte
+
+int readStuff(void) {
+  SPI.beginTransaction(SPISettings(12000000, MSBFIRST, SPI_MODE0));  // gain control of SPI bus
+  digitalWrite(10, SLAVESELECT);         // assert chip select
+  SPI.transfer(0x74);            // send 16 bit command
+  SPI.transfer(0xA2);
+  byte b1 = SPI.transfer(0);     // read 16 bits of data
+  byte b2 = SPI.transfer(0);
+  digitalWrite(10, HIGH);        // deassert chip select
+  SPI.endTransaction();          // release the SPI bus
+  return (int16_t)((b1 << 8) | b2);
 }
 void setup() {
   Serial.begin(9600);
@@ -22,18 +21,10 @@ void setup() {
   pinMode(DATAIN, INPUT);
   pinMode(SPICLOCK, OUTPUT);
   pinMode(SLAVESELECT, OUTPUT);
-  digitalWrite(SLAVESELECT, LOW); //disable device
-  // SPCR = 01010000
-  //interrupt disabled,spi enabled,msb 1st,master,clk low when idle,
-  //sample on leading edge of clk,system clock/4 rate (fastest)
   SPCR = (1 << SPE);
-  clr = SPSR;
-  clr = SPDR;
-  delay(10);
+  
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  int data = spi_transfer(0xFF);
-  //Serial.println(data);
-  delay(10);
+  Serial.println(readStuff());
 }
