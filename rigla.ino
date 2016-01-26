83be4d54f066b084f47d5438224e5544300ecf1b
Konstantin Kavruk <6k6k6k@gmail.com>
2016-01-26 14:34:23 +0200

    Update rigla.ino

--- rigla.ino
+++ rigla.ino
@@ -1,25 +1,20 @@
 //#include <EnableInterrupt.h>
 #define CLOCK 0
-#define MOSI 11
-byte full = 0;
+//#define MOSI 11
+//byte full = 0;
 volatile byte spiVal = 0;
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(CLOCK, INPUT);
   pinMode(MOSI, INPUT);
   attachInterrupt(digitalPinToInterrupt(CLOCK), read, CHANGE);
 }
 
 void loop() {
-  //if (full != 0) 
-  {
-    full = 0;
     Serial.println(spiVal);
-  }
 }
 
 void read() {
   //spiVal |= (spiVal << 1) + digitalRead(MOSI);
   spiVal=17;
-  full++;
 }
