763bc7d0fac52fd70f4e0535ec96d67b773f1ff4
Konstantin Kavruk <6k6k6k@gmail.com>
2016-01-26 12:45:35 +0200

    Update rigla.ino

--- rigla.ino
+++ rigla.ino
@@ -1,21 +1,22 @@
 #define CLOCK 13
 #define MOSI 11
-byte full=0;
-byte spiVal=0;
+byte full = 0;
+byte spiVal = 0;
 void setup() {
-    pinMode(CLOCK, INPUT);
-    pinMode(MOSI,INPUT);
-    attachInterrupt(digitalPinToInterrupt(CLOCK), read, FALLING);
+  Serial.begin(115200);
+  pinMode(CLOCK, INPUT);
+  pinMode(MOSI, INPUT);
+  attachInterrupt(digitalPinToInterrupt(CLOCK), read, FALLING);
 }
 
 void loop() {
-    if (full==7){
-      full=0;
-      Serial.println(spiVal);
-    }
+  if (full == 7) {
+    full = 0;
+    Serial.println(spiVal);
+  }
 }
 
 void read() {
-    spiVal|=(spiVal<<1)+digitalRead(MOSI);
-    full++;
+  spiVal |= (spiVal << 1) + digitalRead(MOSI);
+  full++;
 }
