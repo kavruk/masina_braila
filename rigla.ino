174df0e5a6bc48381bce5f1f5789b3b2db82afcb
Konstantin Kavruk <6k6k6k@gmail.com>
2016-01-28 12:22:24 +0200

    Update rigla.ino

--- rigla.ino
+++ rigla.ino
@@ -1,6 +1,6 @@
 #define CLOCK 0
 #define MOSI 11
-#define DEBUG 0
+#define DEBUG 1
 #define EXTENSION 13
 byte full = 0;
 byte pak = 0;
@@ -23,14 +23,15 @@ void loop() {
     pak++;
     send++;
     full = 0;
-#if DEBUG
-    Serial.print(pak);
-    Serial.print(":");
-    Serial.print(spiVal);
-    Serial.print(" ");
-#endif
+
     if ((pak > 6) && (pak < 17)) {
       sendBuffer[pak - 7] = spiVal;
+      #if DEBUG
+    Serial.print(pak);
+    Serial.print(":");
+    Serial.print(sendBuffer[pak - 7]);
+    Serial.print(" ");
+#endif
     }
   }
   if (20 == pak) {
